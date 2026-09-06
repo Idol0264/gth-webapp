810aa0607b60116346fc6426519aa5942bbaa659
Idol0264 <freedomgodstime749@gmail.com>
2026-09-06 23:53:36 +0100

    Add GTC_ADS array with advertisement data

---
+++ data/gtc-ads.js
@@ -0,0 +1,12 @@
+const GTC_ADS = [
+  {
+    image: "images/gth-ad-1.jpg",
+    title: "GTH Advertisement",
+    link: "#"
+  },
+  {
+    image: "images/gth-ad-2.jpg",
+    title: "GTH Advertisement",
+    link: "#"
+  }
+];
