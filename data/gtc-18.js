522b9c89b2834c0869f976dff1f22d37b00dfe25
Idol0264 <freedomgodstime749@gmail.com>
2026-09-06 23:03:59 +0100

    Add GTC_18 data structure with shorts and full videos

---
+++ data/gtc-18.js
@@ -0,0 +1,77 @@
+const GTC_18 = {
+
+  shorts: [
+
+    {
+      id: "GTC18_SHORT_1",
+      title: "+18 Series Short 1"
+    },
+
+    {
+      id: "GTC18_SHORT_2",
+      title: "+18 Series Short 2"
+    },
+
+    {
+      id: "GTC18_SHORT_3",
+      title: "+18 Series Short 3"
+    },
+
+    {
+      id: "GTC18_SHORT_4",
+      title: "+18 Series Short 4"
+    },
+
+    {
+      id: "GTC18_SHORT_5",
+      title: "+18 Series Short 5"
+    },
+
+    {
+      id: "GTC18_SHORT_6",
+      title: "+18 Series Short 6"
+    }
+
+  ],
+
+
+  fullVideos: [
+
+    {
+      id: "GTC18_FULL_1",
+      title: "+18 Series Full Video 1"
+    },
+
+    {
+      id: "GTC18_FULL_2",
+      title: "+18 Series Full Video 2"
+    },
+
+    {
+      id: "GTC18_FULL_3",
+      title: "+18 Series Full Video 3"
+    },
+
+    {
+      id: "GTC18_FULL_4",
+      title: "+18 Series Full Video 4"
+    },
+
+    {
+      id: "GTC18_FULL_5",
+      title: "+18 Series Full Video 5"
+    },
+
+    {
+      id: "GTC18_FULL_6",
+      title: "+18 Series Full Video 6"
+    },
+
+    {
+      id: "GTC18_FULL_7",
+      title: "+18 Series Full Video 7"
+    }
+
+  ]
+
+};
