c36e1ca40d7e1c18831b88d724e245f283669d2c
Idol0264 <freedomgodstime749@gmail.com>
2026-09-06 23:13:04 +0100

    Add GTC_LOLDAY data structure with shorts and videos

---
+++ data/gtc-lolday.js
@@ -0,0 +1,77 @@
+const GTC_LOLDAY = {
+
+  shorts: [
+
+    {
+      id: "LOLDAY_SHORT_1",
+      title: "L.O.LDay Short 1"
+    },
+
+    {
+      id: "LOLDAY_SHORT_2",
+      title: "L.O.LDay Short 2"
+    },
+
+    {
+      id: "LOLDAY_SHORT_3",
+      title: "L.O.LDay Short 3"
+    },
+
+    {
+      id: "LOLDAY_SHORT_4",
+      title: "L.O.LDay Short 4"
+    },
+
+    {
+      id: "LOLDAY_SHORT_5",
+      title: "L.O.LDay Short 5"
+    },
+
+    {
+      id: "LOLDAY_SHORT_6",
+      title: "L.O.LDay Short 6"
+    }
+
+  ],
+
+
+  fullVideos: [
+
+    {
+      id: "LOLDAY_FULL_1",
+      title: "L.O.LDay Full Video 1"
+    },
+
+    {
+      id: "LOLDAY_FULL_2",
+      title: "L.O.LDay Full Video 2"
+    },
+
+    {
+      id: "LOLDAY_FULL_3",
+      title: "L.O.LDay Full Video 3"
+    },
+
+    {
+      id: "LOLDAY_FULL_4",
+      title: "L.O.LDay Full Video 4"
+    },
+
+    {
+      id: "LOLDAY_FULL_5",
+      title: "L.O.LDay Full Video 5"
+    },
+
+    {
+      id: "LOLDAY_FULL_6",
+      title: "L.O.LDay Full Video 6"
+    },
+
+    {
+      id: "LOLDAY_FULL_7",
+      title: "L.O.LDay Full Video 7"
+    }
+
+  ]
+
+};
