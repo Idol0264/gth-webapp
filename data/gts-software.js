45e141a4982cc94186b95f46820470e5a865d50e
Idol0264 <freedomgodstime749@gmail.com>
2026-09-07 10:39:36 +0100

    Add GTS_SOFTWARE data structure

---
+++ data/gts-software.js
@@ -0,0 +1,17 @@
+const GTS_SOFTWARE = [
+  {
+    name: "Example Software",
+    category: "UTILITY",
+    description: "A useful digital application on the GTS platform.",
+    image: "images/software-placeholder.jpg",
+    page: "gts-software.html"
+  },
+
+  {
+    name: "Example App",
+    category: "PRODUCTIVITY",
+    description: "A productivity application available through GTS.",
+    image: "images/software-placeholder.jpg",
+    page: "gts-software.html"
+  }
+];
